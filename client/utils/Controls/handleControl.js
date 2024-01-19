04d0bd28edbfe2eb30c34e68300d0d794756e815
Tom Dent <tomdent991@hotmail.com>
2024-01-19 12:02:59 +0100

    ugly but works

--- client/utils/Controls/handleControl.js
+++ client/utils/Controls/handleControl.js
@@ -7,15 +7,9 @@ export default function handleControl(event, sendReceive, mixerArray) {
       value: event.value,
     });
   }
-  // const channel = event.name.split("-")[0];
-  // const channelCaps = channel.charAt(0).toUpperCase() + channel.slice(1);
-  // const type = event.name.split("-")[1];
-  // const objectName = type + channelCaps;
-  // const param = event.name.split("-")[2];
-  // const nodeObject = mixerArray.current.find(
-  //   (obj) => obj.current.name === objectName
-  // );
+
   const node = getNodeFromMixerArray(event, mixerArray)
+  const param = event.name.split("-")[2];
 
   node.current[param].value = event.value;
   console.log(node.current[param].value)
@@ -28,10 +22,9 @@ function getNodeFromMixerArray(event, mixerArray) {
   const channelCaps = channel.charAt(0).toUpperCase() + channel.slice(1);
   const type = event.name.split("-")[1];
   const objectName = type + channelCaps;
-  const param = event.name.split("-")[2];
   const node = mixerArray.current.find(
     (obj) => obj.current.name === objectName
   );
 
-  return nodeObject
+  return node
 }
